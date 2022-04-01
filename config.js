0e34c579a0afbf1a93a96eca8b725cfce44bea90
dungvtph09661 <61072709+dungvtph09661@users.noreply.github.com>
2022-04-01 18:14:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tho",
+    introTitle: 'Huong Ly',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Click^^',
+    title: 'Anh đã suy nghĩ nên tiếp tục hay dừng lại nghĩ cho cảm nhận em ',
+    desc: 'Việc anh cố chấp mang lại được kết quả gì ko hay nhận được sự thờ ơ',
+    btnYes: 'Nên cố gắng 💪💪',
+    btnNo: 'Dừng lại trả lại csong cho e',
+    question: 'Trên thế giới hơn 7 tỉ người khi anh đủ sức và sự cố gắng',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Mọi sự cố gắng a mong giữ được em thôi nếu sự cố gắng anh mà làm e khó chịu phiền thì ...',
+    mess: 'Em thể nói em thấy khó chịu hay phiền khi a làm ảnh hưởng cuộc sống em không ???',
+    messDesc: 'Câu nói em là lý do và kết quả a nên hay không nên nữa',
+    btnAccept: 'Cick và ib vào cho a ',
+    messLink: 'https://www.facebook.com/moonylanhlung' 
+}
